6da6f6dbe1fe9b52ad6a759ddd307e8ebf59c6bd
Max Henstell <max.henstell@makerbot.com>
2014-12-08 17:57:22 -0500

    support multiple images maybe

--- Hardware/gpmcTest/imageConverter.py
+++ Hardware/gpmcTest/imageConverter.py
@@ -1,24 +1,48 @@
 from PIL import Image
 import sys, os
 
-openFile = sys.argv[1]
-filename = os.path.split(openFile)[1]
-filebase = os.path.splitext(filename)[0]
+openFolder = sys.argv[1]
+files = os.listdir(openFolder)
 
-myImage = Image.open(openFile).convert("RGB")
-pixels = myImage.load()
+minImage = 0
+maxImage = 0
 
-sixteenBPP = []
+images = []
+ext = os.path.splitext(files[0])[1]
+
+basenames = []
+imageNames = {}
+images = {}
+
+def imageTo16BPP(image):
+	sixteenBPP = []
+	pixels = image.load()
+	for y in range(image.size[1]):
+		for x in range(image.size[0]):
+			r = pixels[x, y][0] >> 3
+			g = pixels[x, y][1] >> 2
+			b = pixels[x, y][2] >> 3
+			rgb = (r << 11) + (g << 5) + b
+			
+			hi, lo = split(rgb)
+
+			hiBits, loBits = []
+			for bit in range(8):
+				hiBits.append((hi >> 7-bit) & 1 > 0)
+				loBits.append((lo >> 7-bit) & 1 > 0)
+
+			sixteenBPP.append(hiBits)
+			sixteenBPP.append(loBits)
+	return sixteenBPP
+
+def split(byte16):
+	hi = byte16 >> 8
+	lo = byte16 & 255
+	return hi, lo
+
 frame = bytearray()
 output = bytearray()
 
-for y in range(myImage.size[1]):
-	for x in range(myImage.size[0]):
-		r = pixels[x, y][0] >> 3
-		g = pixels[x, y][1] >> 2
-		b = pixels[x, y][2] >> 3
-		out = (r << 11) + (g << 5) + b
-		sixteenBPP.append(out)
 
 # for x in range(320 * 240 * 8):
 # 	if x % 2 == 0:
@@ -35,18 +59,57 @@ for y in range(myImage.size[1]):
 # 	frame.append(hi)
 # 	frame.append(lo)
 
-print("Length sixteenBPP %s" % len(sixteenBPP))
-for byte in sixteenBPP:
-	for i in range(16):
-		if (byte >> 15-i) & 1 > 0:
-			output.append(255)
-			output.append(255)
-		else:
-			output.append(0)
-			output.append(0)
+# print("Length sixteenBPP %s" % len(sixteenBPP))
+# for byte in sixteenBPP:
+# 	for i in range(16):
+# 		if (byte >> 15-i) & 1 > 0:
+# 			output.append(255)
+# 			output.append(255)
+# 		else:
+# 			output.append(0)
+# 			output.append(0)
 
-print("Length output %s" % len(output))
-with open(filebase + ".bin", "wb") as bin:
-	bin.write(output)	
+#
+
+if __name__ == "__main__":
+
+	# Get basenames, min and max image numbers (format: image-0.bmp)
+	for filename in files:
+		filename = os.path.splitext(filename)[0]
+		num = int(filename.split('-')[1])
+		minImage = min(minImage, num)
+		maxImage = max(maxImage, num)
+
+		basename = filename.split('-')[0]
+		if basename not in basenames:
+			basenames.append(basename)
+
+	# Compile dict of image numbers vs basenames
+	for num in range(minImage, maxImage + 1):
+		imageNames[num] = [basename for basename in basenames]
+
+	# Open the images, get 16 bpp pixel list, append it to images dict
+	for num, basenames in imageNames.items():
+		images[num] = []
+		for basename in basenames:
+			im = Image.open(openFile).convert("RGB")
+			sixteenBPP = imageTo16BPP(im)
+			images[num].append(sixteenBPP)
+
+	for num, pixelList in images.items():
+		zippedPixels = [0 for 0 in (320 * 240 * 16 * 2)]
+		numImages = len(pixelList)
+
+		for byte8 in len(zippedPixels):
+			for image in pixelList:
+				for x in range(8)
+					zippedPixels[byte8] <<= 1
+					zippedPixels[byte8] & pixelList[byte8][x]
+
+ # 	print("Length output %s" % len(zippedPixels))
+	# with open(filebase + ".bin", "wb") as bin:
+	# 	bin.write(zippedPixels)	
+
+
 
 
