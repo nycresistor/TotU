01fd868b8c7fd231a6cf176e1fb4cbf55f581550
Justin Day <justin.day@gmail.com>
2015-02-03 12:28:29 -0500

    cleaned up, backgroud texture, padding

--- js/totuemu.js
+++ js/totuemu.js
@@ -1,37 +1,63 @@
 var TOTUEMU = (function() {
+	var config = {
+		screen: { width: 320, height: 240, paddingLeft: 160, paddingTop: 120 },
+		panel: { paddingBottom: 500, paddingTop: 50, paddingLeft: 0, paddingRight: 0 }
+	};
+
 	var exports = {};
+	var textureImage;
 	var texture;
 	var firebase;
 
 	function Screen(paneltexture, n) {
-		this.subno = Math.floor(Math.random()*512);
-		this.y = Math.floor(n / 4) * 240;
-		this.x = (n % 4) * 320
-		this.n = 0;
-		this.pantex = paneltexture;
 		var self = this;
+
+		this.subno = Math.floor(Math.random()*512);
+		this.pantex = paneltexture;
+		this.n = 0;
+
+		this.y = Math.floor(n / 4) * config.screen.height + 
+			config.screen.paddingTop * (Math.floor(n / 4) + 1) +
+			config.panel.paddingTop;
+
+		this.x = (n % 4) * config.screen.width + 
+			config.screen.paddingLeft * ((n % 4 )+ 1) +
+			config.panel.paddingLeft;
 
 		this.paint = function paint() {
 			if (TOTUEMU.submissions.length) {
 				var sub = self.subno % TOTUEMU.submissions.length;
 				var photos = TOTUEMU.submissions[sub].photos;
 
-				self.pantex.ctx.drawImage(photos[self.n], self.x, self.y);
+				self.pantex.ctx.drawImage(photos[self.n], self.x, self.y, config.screen.width, config.screen.height);
 				self.n++;
 				if (self.n>=photos.length) self.n=0;
 
-				self.pantex.texture.needsUpdate = true;
 			}
+			else {
+				self.pantex.ctx.fillStyle = "black";
+				self.pantex.ctx.fillRect(self.x, self.y, config.screen.width, config.screen.height);
+			}
+
+			self.pantex.texture.needsUpdate = true;
 		}
 	}
 
 	function PanelTexture(panel,side) {
-		this.canvas = $('<canvas width="1280" height="1920" class="texture"></canvas>')[0];
+		var canvasWidth = (config.screen.width + config.screen.paddingLeft) * 4 + 
+			config.screen.paddingLeft + config.panel.paddingLeft + config.panel.paddingRight;
+		var canvasHeight = (config.screen.height + config.screen.paddingTop) * 8 + 
+			config.screen.paddingTop + config.panel.paddingTop + config.panel.paddingBottom;
+
+		this.canvas = $('<canvas width="' + canvasWidth + '" height="' + canvasHeight + '" class="texture"></canvas>')[0];
 		$('#page-top').append(this.canvas);
 		this.ctx = this.canvas.getContext('2d');
 
-		this.ctx.fillStyle = side ? "red" : "green";
-		this.ctx.fillRect(0,0,1280,1920);
+		for (var x=0; x<canvasWidth; x+=textureImage.width) {
+			for (var y=0; y<canvasHeight; y+=textureImage.height) {
+				this.ctx.drawImage(textureImage,x,y);
+			}
+		}
 
 		this.texture = new THREE.Texture(this.canvas);
 		this.texture.needsUpdate = true;
@@ -39,6 +65,7 @@ var TOTUEMU = (function() {
 		this.screens = [];
 		for (var i=0; i<32; i++) {
 			this.screens.push(new Screen(this, i));
+			this.screens[i].paint();
 		}
 
 		var self = this;
@@ -75,7 +102,7 @@ var TOTUEMU = (function() {
 	};
 	
 	function init3d() {
-		texture = THREE.ImageUtils.loadTexture('/img/texture-wood.jpg')
+		texture = THREE.ImageUtils.loadTexture('/img/texture-wood.jpg');
 		var scene = new THREE.Scene();
 		var camera = new THREE.PerspectiveCamera( 75, $('#virtual').width() / $('#virtual').height(), 0.1, 1000 );
 		var renderer = new THREE.WebGLRenderer({ canvas: $('#virtual')[0] });
@@ -133,8 +160,12 @@ var TOTUEMU = (function() {
 
 	exports.init = function() {
 		if ($('#virtual').length) {
-			initDb();
-			init3d();
+			textureImage = new Image();
+			textureImage.onload = function() {
+				initDb();
+				init3d();
+			}
+			textureImage.src = "/img/texture-wood.jpg";
 		}
 	}
 
