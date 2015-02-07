0f75906a69e4bb8c198ea709adb23abbc51bb734
Max Henstell <max.henstell@makerbot.com>
2015-02-07 18:43:51 -0500

    Added slicer support for dual screen banks

--- Firmware/image_slicer/slicer.c
+++ Firmware/image_slicer/slicer.c
@@ -3,20 +3,24 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>     /* strcat */
-#include <dirent.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-uint16_t imageData[16][76800];
-const char * outputfolder = "seq";
+#define SCREENS_PER_BANK 16
 
-void readImages(char * inputfolder, int framenumber)
+uint16_t imageData[SCREENS_PER_BANK][76800];
+
+void readImages(char * inputfolder, int framenumber, int bankNum)
 {
 	uint8_t image[230400];
+	char bank = bankNum == 0 ? 'A' : 'B';
 
-	for (int i = 0; i < 16; i++)
-	{		
+	for (int screen = 0; screen < SCREENS_PER_BANK; screen++)
+	{	
+		// Open the file at inputfolder/framenumber-i.rgb
 		char filename[100];
-		sprintf(filename, "%s/%02d-%02d.rgb", inputfolder, framenumber, i);
+		sprintf(filename, "%s/%02d-%c%02d.rgb", inputfolder, framenumber, bank, screen);
 		printf("Reading from %s\n", filename);
 
 		if (access(filename, F_OK) == -1) {
@@ -24,10 +28,11 @@ void readImages(char * inputfolder, int framenumber)
 			exit(-1);
 		}
 
+		// Read the file into the image array
 		FILE * filePointer = fopen(filename, "rb");
-
 		fread(image, sizeof(uint8_t), 230400, filePointer);
 
+		// Iterate through each pixel, pull out each color
 		for (int q = 0; q < 76800; q++)
 		{
 			uint8_t r = image[(q * 3) + 0];
@@ -37,10 +42,12 @@ void readImages(char * inputfolder, int framenumber)
 			// uint8_t hi = (r & 0xF8) + (g >> 5);
 			// uint8_t lo = (g & 0x1C) + ((b & 0xF8) >> 3);
 
+			// Convert to a 16-bit integer
 			uint8_t hi = ((r >> 3) << 3) | (g >> 5);
 			uint8_t lo = (g << 5) | (b >> 3);
 
-			imageData[i][q] = (hi << 8) | lo;
+			// Throw the 16-bit int into the imageData array
+			imageData[screen][q] = (hi << 8) | lo;
 		}
 
 		fclose(filePointer);
@@ -63,6 +70,7 @@ const char *byte_to_binary(int x)
 
 void outputBinary(char * filename)
 {
+	// Open the outputFile
 	FILE * outputFile = fopen(filename, "wb");
 
 	for (int byte = 0; byte < 76800; byte++)
@@ -74,7 +82,6 @@ void outputBinary(char * filename)
 		}
 
 		uint16_t outArray[16];
-
 		for (int q = 0; q < 16; q++)
 		{
 			uint16_t outByte = 0;
@@ -96,42 +103,56 @@ void outputBinary(char * filename)
 
 int main (int argc, char *argv[])
 {	
+
+	// Check that an input folder was provided
 	char * inputfolder = argv[1];
 	if (inputfolder == NULL)
 	{
 		printf("Input folder not specified.\n");
 		exit(-1);
 	}
-	if (argv[2] != NULL)
+
+	// Check that an output folder was provided
+	char * outputfolder = argv[2];
+	if (argv[2] == NULL)
 	{
-		outputfolder = argv[2];
-	}
-	if (access(outputfolder, F_OK) == -1) {
-		printf("Output folder %s doesn't exist.\n", outputfolder);
-		exit(-1);
+		printf("Output folder not specified.\n");
 	}
 
-	int numframes = 0;
-	for (int i = 0; i < 100; i++)
+	// Check that the output folder exists or create it
+	if (access(outputfolder, F_OK) == -1) {
+		if (mkdir(outputfolder, 0777) == -1) {
+			printf("Error creating output folder %s: %d\n", outputfolder, errno);
+			exit(-1);
+		}
+	}
+
+	// This only checks to see if the first screen of
+	// each frame exists, does not check for all screens
+	int numFrames = 0;
+	for (int frame = 0; frame < 100; frame++)
 	{
 		char filename[100];
-		sprintf(filename, "%s/%02d-00.rgb", inputfolder, i);
+		sprintf(filename, "%s/%02d-A00.rgb", inputfolder, frame);
 		if (access(filename, F_OK) != -1) {
-			numframes = i + 1;
+			numFrames = frame + 1;
 		} else {
 			break;
 		}
 	}
 
-	printf("Found %d frames\n", numframes);
+	printf("Found %d frames\n", numFrames);
 
-	for (int frameno = 0; frameno < numframes; frameno++)
+	for (int frame = 0; frame < numFrames; frame++)
 	{
-		readImages(inputfolder, frameno);
-		char filename[100];
-		sprintf(filename, "%s/%02d.bin", outputfolder, frameno);
-		outputBinary(filename);
+		for (int bankNum = 0; bankNum < 2; bankNum++){
+
+			char bank = bankNum == 0 ? 'A' : 'B';
+			readImages(inputfolder, frame, bankNum);
+			char filename[100];
+			sprintf(filename, "%s/%02d-%c.bin", outputfolder, frame, bank);
+			outputBinary(filename);
+
+		}	
 	}
-	
-	
 }
--- Firmware/image_slicer/slicer.h
+++ Firmware/image_slicer/slicer.h
@@ -1,7 +1,7 @@
 #ifndef _slicer_h_
 #define _slicer_h_
 
-void readImages(char * inputfolder, int framenumber);
+void readImages(char * inputfolder, int framenumber, int bank);
 const char *byte_to_binary(int x);
 void outputBinary(char * filename);
 
